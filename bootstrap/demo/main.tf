3e2cb555c9b39708df9494e8daba590600a83be5
Anton Babenko <anton@antonbabenko.com>
2024-06-07 11:36:15 +0200

    Added iam-github-oidc-provider

--- bootstrap/demo/main.tf
+++ bootstrap/demo/main.tf
@@ -10,6 +10,11 @@ provider "aws" {
   }
 }
 
+module "iam_github_oidc_provider" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-github-oidc-provider"
+  version = "~> 5.0"
+}
+
 module "iam_github_oidc_role" {
   source  = "terraform-aws-modules/iam/aws//modules/iam-github-oidc-role"
   version = "~> 5.0"
