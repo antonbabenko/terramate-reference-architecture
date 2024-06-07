13d00d029eaa99ff7f1edb6a03bd991002eb0e0c
Anton Babenko <anton@antonbabenko.com>
2024-06-07 11:44:41 +0200

    Run tg automatically

--- stacks/dev/multiple-buckets/for_each_main.tf
+++ stacks/dev/multiple-buckets/for_each_main.tf
@@ -12,7 +12,7 @@ module "s3_bucket" {
   object_ownership = "ObjectWriter"
   source           = "terraform-aws-modules/s3-bucket/aws"
   tags = {
-    Env   = "dev123"
+    Env   = "dev"
     Stack = "dev-multiple-buckets"
   }
   version = "~> 4.0"
